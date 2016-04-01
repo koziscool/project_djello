a1ecf46d9b432af3be087adc6ebbe676d60c50d8
koziscool <kozjob@yahoo.com>
2016-04-01 15:49:44 -0500

    bugfix

--- app/models/board.rb
+++ app/models/board.rb
@@ -2,7 +2,6 @@ class Board < ActiveRecord::Base
   
   has_many :board_memberships
 
-
   has_many :lists
   belongs_to :user
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,10 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :board_memberships
-  has_many :boards, :through => board_memberships:
+  has_many :boards, :through => :board_memberships
 
   has_many :card_memberships, :through => :board_memberships
-  has_many :cards, :thorugh => :card_membersships
+  has_many :cards, :through => :card_memberships
 
 
   has_many :boards
