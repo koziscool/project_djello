f898e77d73882d75f8e2c9aae7d55d669f19dbe8
koziscool <kozjob@yahoo.com>
2016-04-01 13:04:53 -0500

    stuff

--- app/assets/javascripts/controllers/boardController.js
+++ app/assets/javascripts/controllers/boardController.js
@@ -1,5 +1,8 @@
 
 djelloApp.controller('BoardCtrl', ['boardService', '$scope',  '$stateParams',function(boardService, $scope, $stateParams){
 
+  boardService.getIndex();
+  $scope.boards = boardService.index;
+  $scope.currentBoard = $scope.boards[0];
 
 }]);
